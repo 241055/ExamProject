326df8fdf6654848f495c3b5c11266b24ea66de0
SakodaTaiga <241055@std.hi-joho.ac.jp>
2025-07-18 12:08:16 +0900

    s

--- src/Slime.java
+++ src/Slime.java
@@ -1,7 +1,6 @@
 public final class Slime extends Monster {
-    public Slime(char suffix, int hp) {
-        super(suffix,hp);
-        super.setName("スライム");
+    public Slime(String name,char suffix, int hp) {
+        super(name,suffix,hp);
     }
     public void attack(Creature target) {
         System.out.println(this.getName() + "は体当たり攻撃!" + target.getName() + "に5のダメージを与えた!");
